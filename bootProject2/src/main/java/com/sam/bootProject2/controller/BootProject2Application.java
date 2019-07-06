b09a132ee8fdbbc68a63978332c67ca01fbace79
Sameer Milon Nayak <50694386+sameermilon829@users.noreply.github.com>
2019-07-06 17:18:09 +0530

    Add files via upload

---
+++ bootProject2/src/main/java/com/sam/bootProject2/controller/BootProject2Application.java
@@ -0,0 +1,33 @@
+package com.sam.bootProject2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sam.bootProject2.service.EmployeeService;
+
+
+@SpringBootApplication(scanBasePackages={"com.sam.bootProject2"})
+@RestController
+@EnableAutoConfiguration
+public class BootProject2Application {
+
+	@Autowired
+	private EmployeeService employeeService;	
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(BootProject2Application.class, args);
+	}
+
+	
+	@RequestMapping("/")
+	public @ResponseBody String working(){
+		return employeeService.fetchEmployeeDetails(1);
+	}
+	
+}
---
+++ bootProject2/src/main/java/com/sam/bootProject2/controller/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.sam.bootProject2.controller;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BootProject2Application.class);
+	}
+
+}
